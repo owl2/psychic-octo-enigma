95d1834ec19173666ad0f8596f6a330e515639bf
lmu <l.martineau971@gmail.com>
2021-06-25 15:42:39 +0200

    Work on S3 part

--- code/ingest/s3_put.py
+++ code/ingest/s3_put.py
@@ -1,13 +1,33 @@
+import os
 import boto3
-
 
 
 s3 = boto3.resource('s3')
 
-# Print all bucket name
-for bucket in s3.buckets.all(): 
-    print(bucket.name)
 
 
-data = open('../../data/test.csv', 'rb')
-s3.Bucket('20210317bucket').put_object(Key='test.csv', Body=data)
+def print_bucket_s3():
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
+
+
+
+if __name__ == "__main__": 
+    # For test
+    path = os.getcwd()
+    print(path)
+
+
+    #List all the buckets in S3
+    print_bucket_s3()
+
+
+    #Create a a bucket for the test
+    #TODO
+
+    try: 
+        data = open('data/test.csv', 'rb')
+        s3.Bucket('20210317bucket').put_object(Key='test.csv', Body=data)
+    except ValueError:
+        print("Oops, there is a probleme with to import de the files in S3")
